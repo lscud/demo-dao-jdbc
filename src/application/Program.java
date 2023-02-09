b6fad68e2956de9674522d963769994bea2025c1
Luis Antonio <luisscudeler@yahoo.com.br>
2023-02-09 09:37:09 -0300

    SellerDaoJdbc and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,7 @@ public class Program {
 		
 		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
+		SellerDao sellerDao = DaoFactory.crateSellerDao(); //dessa forma o eu programa nao cohece a implementação ele sonhece somente a interface 
 		System.out.println(seller);
 		
 
