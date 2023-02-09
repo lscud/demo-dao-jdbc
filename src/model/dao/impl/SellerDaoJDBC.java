f68d986cad84e367a93a0f7730b514054e87acfa
Luis Antonio <luisscudeler@yahoo.com.br>
2023-02-09 13:44:20 -0300

    findAll imlementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -94,8 +94,44 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> finddAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "ORDER BY Name ");
+			
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>(); 
+			
+			while (rs.next()) {
+				Department dep = map.get(rs.getInt("DepartmentId")); // vai retornar nulo caso nao tenha o id buscado na coluna Department
+				
+				//o if abaixo é para nao criar um department para cada vendedor. Dessa forma estaria errado. O correto é um departamento para varios vendedores (pois no mesmo departamnto posso ter varios vendedores
+				if (dep == null) { //se o departamento ja existir vou aproveitar o existente se não vou instanciar um novo
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep); 
+				}
+				
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+				
+			}
+			return list;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+			
+			
+		}
 	}
 
 	@Override
