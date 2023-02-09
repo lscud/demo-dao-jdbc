ceee6b169adb5f51ee5e27b0c245c3b3314ee114
Luis Antonio <luisscudeler@yahoo.com.br>
2023-02-09 10:28:16 -0300

    instanciaing obj - improving code

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,6 +13,7 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao(); //dessa forma o eu programa nao cohece a implementação ele sonhece somente a interface
 		
+		System.out.println("=== TEST 1: seller findById ======== ");
 		Seller seller = sellerDao.findById(3); 
 		
 		System.out.println(seller);
