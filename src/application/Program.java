cbfd7dfd49fde3be48118729cc52ac557ef06f32
Luis Antonio <luisscudeler@yahoo.com.br>
2023-02-09 15:07:48 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,8 +36,12 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
+		System.out.println("\n=== TEST 5: seller Insert ======== ");
+		seller = sellerDao.findById(1);
+		seller.setName("MArta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 		
-
 	}
 
 }
